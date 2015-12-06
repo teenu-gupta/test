891ba89c63fd7742e8e3b2eeb3ee4f1cb93445b7
teenu-gupta <teenu.gupta@jaarvis.com>
2015-12-06 16:23:05 +0530

    Engine settings database

--- library/settings.py
+++ library/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default':{
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'myproject',
         'USER': 'teenu',
         'PASSWORD': 'test1234',
